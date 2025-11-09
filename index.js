ca4c7181c0ed2635e117bf308e4ffc60cdda0f57
alihasna32 <muktermukter555@gmail.com>
2025-11-09 16:30:22 +0600

    verifyFirebaseToken added

--- index.js
+++ index.js
@@ -3,12 +3,35 @@ const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
+const admin = require("firebase-admin");
+const serviceAccount = require("./freelance-market-token-verify.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 app.use(cors());
 app.use(express.json());
 
-//freelance-market-place
-//XfdCDCk9tekBk75z
+const verifyFirebaseToken = async(req, res, next) => {
+    const authorization = req.headers.authorization
+    if(!authorization){
+        return res.status(401).send({message: "unauthorized access"})
+    }
+    const token = authorization.split(' ')[1];
+    if(!token){
+        return res.status(401).send({message: "unauthorized access"})
+    }
+
+    try{    
+        const decoded = await admin.auth().verifyIdToken(token)
+        req.token_email = decoded.email
+        next()
+    }       
+    catch(error){
+        return res.status(401).send({message: "unauthorized access"})
+    }
+}
 const uri =
   "mongodb+srv://freelance-market-place:XfdCDCk9tekBk75z@cluster0.tiedhl1.mongodb.net/?appName=Cluster0";
 
@@ -31,7 +54,7 @@ async function run() {
     const jobcollection = db.collection("jobs");
     const acceptedTasksCollection = db.collection("acceptedTask")
 
-    app.post("/addJob", async (req, res) => {
+    app.post("/addJob", verifyFirebaseToken, async (req, res) => {
       const newjob = req.body;
       const result = await jobcollection.insertOne(newjob);
       res.send(result);
@@ -42,14 +65,14 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/allJobs/:id", async (req, res) => {
+    app.get("/allJobs/:id", verifyFirebaseToken, async (req, res) => {
       const { id } = req.params;
       const query = { _id: new ObjectId(id) };
       const result = await jobcollection.findOne(query);
       res.send(result);
     });
 
-    app.patch("/updateJob/:id", async (req, res) => {
+    app.patch("/updateJob/:id", verifyFirebaseToken, async (req, res) => {
       const { id } = req.params;
       const updatedJobs = req.body;
       const query = { _id: new ObjectId(id) };
@@ -65,26 +88,26 @@ async function run() {
       res.send(result);
     });
 
-    app.delete("/deleteJob/:id", async(req, res) => {
+    app.delete("/deleteJob/:id", verifyFirebaseToken, async(req, res) => {
         const {id} = req.params
         const deleteJob = {_id: new ObjectId(id)} 
         const result = await jobcollection.deleteOne(deleteJob)
         res.send(result)
     })
 
-    app.get("/myAddedJobs", async(req, res) => {
+    app.get("/myAddedJobs", verifyFirebaseToken, async(req, res) => {
         const email = req.query.email
         const result = await jobcollection.find({userEmail: email}).toArray()
         res.send(result)
     })
 
-    app.post("/accept-task", async(req, res) => {
+    app.post("/accept-task", verifyFirebaseToken, async(req, res) => {
         const accepted = req.body
         const result = await acceptedTasksCollection.insertOne(accepted)
         res.send(result)
     })
     
-    app.get("/my-accepted-tasks", async(req, res) => {
+    app.get("/my-accepted-tasks", verifyFirebaseToken, async(req, res) => {
         const email = req.query.email;
         const result = await acceptedTasksCollection.find({userEmail: email}).toArray()
         res.send(result)
