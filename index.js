f36c17305d5e377ec072ecc66c896b039cca5926
alihasna32 <muktermukter555@gmail.com>
2025-11-09 11:32:04 +0600

    allJobs api route

--- index.js
+++ index.js
@@ -38,7 +38,10 @@ async function run() {
       res.send(result);
     });
 
-    
+    app.get("/allJobs", async(req, res) => {
+        const result = await jobcollection.find().toArray()
+        res.send(result)
+    })
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
