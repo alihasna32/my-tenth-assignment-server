2d854904f5c2ca686bceed54a86f738ff3a3b608
alihasna32 <muktermukter555@gmail.com>
2025-11-09 11:30:49 +0600

    addJob api route

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express()
+const port = process.env.PORT || 3000
+
+
+app.use(cors());
+app.use(express.json());
+
+//freelance-market-place
+//XfdCDCk9tekBk75z
+const uri = "mongodb+srv://freelance-market-place:XfdCDCk9tekBk75z@cluster0.tiedhl1.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db("freelance-market-place");
+    const jobcollection = db.collection("jobs")
+
+
+    app.post("/addJob", async (req, res) => {
+      const newjob = req.body;
+      const result = await jobcollection.insertOne(newjob);
+      res.send(result);
+    });
+
+    
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
